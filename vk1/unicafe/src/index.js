199364f5793c4ffcf9299b7cd3c695072b991e9a
Antiik91 <antiik91@gmail.com>
2018-01-22 16:20:07 +0200

    1.12

--- vk1/unicafe/src/index.js
+++ vk1/unicafe/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component{
   constructor(props) {
-
     super(props)
     this.state = {
       good: 0,
@@ -41,22 +40,23 @@ class App extends React.Component{
     )
     const Statistic =({statistic,text}) => {
       if(text==='positiivisia') {
-        return (<p> {text} {statistic} %</p>)
+        return (<tr><td> {text} {statistic} %</td></tr>)
       }
-      return (<p> {text} {statistic} </p>)
+      return (<tr><td> {text} {statistic} </td></tr>)
     }
     const Statistics = ({stats,total}) => {
       if(total===0){
-        return <div> ei yhtään palautetta annettu </div>
+        return (<table><tbody><tr><td>ei yhtään palautetta annettu</td></tr>
+        </tbody></table>)
       }
-      return (
-        <div>
+      return ( <table><tbody>
           <Statistic text={stats[0].name} statistic={stats[0].value} />
           <Statistic text={stats[1].name} statistic={stats[1].value} />
           <Statistic text={stats[2].name} statistic={stats[2].value} />
           <Statistic text={stats[3].name} statistic={stats[3].value} />
           <Statistic text={stats[4].name} statistic={stats[4].value} />
-        </div>
+        </tbody>
+        </table>
         )
     }
     const total = () => this.state.bad + this.state.good + this.state.neutral
@@ -77,23 +77,23 @@ class App extends React.Component{
       {
       name: 'hyvä',
       value: this.state.good
-    },
-    {
+      },
+      {
       name: 'neutraali',
       value: this.state.neutral
-    },
-    {
+      },
+      {
       name:'huono',
       value: this.state.bad
-    },
-    {
+      },
+      {
       name: 'keskiarvo',
       value: average()
-    },
-    {
+      },
+      {
       name: 'positiivisia',
       value: positive()
-    }
+      }
     ]
     return (
       <div>
@@ -107,10 +107,9 @@ class App extends React.Component{
             <Button
               handleClick={this.click("bad",this.state.bad +1)}
               text="huono" />
-
         <h3> statistiikka </h3>
-         <Statistics stats={stats} total={total()}/>
-      </div>
+           <Statistics stats={stats} total={total()}/>
+        </div>
     )
   }
 }
